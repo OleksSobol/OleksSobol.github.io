4c87f9d6cf7afc49603b6c620b6e63eab941070a
OleksSobol <olekssobol@gmail.com>
2018-05-05 12:38:59 -0600

    50%

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,8 +10,8 @@ $(document).on('click', 'a[href^="#"]', function (event) {
 $('.img-carousel').slick({
     dots: true,
     infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToShow: 4,
+    slidesToScroll: 4,
     variableWidth: true,
     //   autoplay: true,
     //   autoplaySpeed: 2500,
@@ -59,3 +59,30 @@ particlesJS.load('particles-js', 'assets/js/particles.json', function () {
     console.log('callback - particles.js config loaded');
 });
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+
+
+// animation
+const contact = document.querySelector("#contact_menu");
+
+contact.addEventListener("click",function() {
+    animate();
+});
+
+function animate() {
+    anime({
+        targets: '.social-network ul li a img ',
+        scale: {value: 1.2, duration: 2500},
+        // paddingRight: 50,
+        marginTop: 20,
+        delay: 900,
+        translateX: -50,
+        direction: 'alternate',
+        // loop: true,
+        delay: function(el, i, l) {
+            return 500 + (i * 500);
+        }
+    });
+
+
+}
+// end of animation
